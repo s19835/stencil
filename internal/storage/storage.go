a482c1e7eca841d394604dadc1bd1823e84806dd
s19835 <s19835@sci.pdn.ac.lk>
2025-07-12 15:59:10 +0530

    add: storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,47 @@
 package storage
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/s19835/stencil/internal/model"
+)
+
+func getSnippetsDir() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	dir := filepath.Join(home, ".stencil", "snippets")
+
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0755)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return dir, nil
+}
+
+func SaveSnippet(s *model.Snippet) error {
+	dir, err := getSnippetsDir()
+	if err != nil {
+		return err
+	}
+
+	filePath := filepath.Join(dir, fmt.Sprintf("%s.json", s.ID))
+	f, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	enc := json.NewEncoder(f)
+	enc.SetIndent("", " ")
+	return enc.Encode(s)
+}
