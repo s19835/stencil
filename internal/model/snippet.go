49194cda8ac080990c6d98db8c5acf56c6d34a38
s19835 <s19835@sci.pdn.ac.lk>
2025-07-12 14:56:12 +0530

    add: snippet.go

--- internal/model/snippet.go
+++ internal/model/snippet.go
@@ -1,1 +1,16 @@
 package model
+
+import (
+	"time"
+)
+
+type Snippet struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Content     string    `json:"content"`
+	Language    string    `json:"language"`
+	Tags        []string  `json:"tags"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
